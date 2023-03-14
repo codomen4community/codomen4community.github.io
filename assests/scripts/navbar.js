a7db19ca4fb9c6660e2a4714476d2cb9e7109c7a
rohitbutbliss <kumarrohit6324@gmail.com>
2023-03-14 14:41:48 +0530

    fixed styling a little

--- assests/scripts/navbar.js
+++ assests/scripts/navbar.js
@@ -1,21 +1,23 @@
 function navbar() {
   return `
-  <div id="logo">
-  <img src="./assests/images/codomen-logo.png" alt="" />
-  <span id="logo-text">For the beginners, by the beginners</span>
-</div>
+  <div id="main-container">
+        <div id="logo">
+          <img src="./assests/images/codomen-logo.png" alt="" />
+          <span id="logo-text">For the beginners, by the beginners</span>
+        </div>
 
-<div id="menu">
-  <ul>
-    <li><a href="#about">About Us</a></li>
-    <li>
-      <a href="https://github.com/codomen4community" target="_blank"
-        >GitHub</a
-      >
-    </li>
-    <li><a href="#projects">Projects</a></li>
-  </ul>
-</div>`;
+        <div id="menu">
+          <ul>
+            <li><a href="#about">About Us</a></li>
+            <li>
+              <a href="https://github.com/codomen4community" target="_blank"
+                >GitHub</a
+              >
+            </li>
+            <li><a href="#projects">Projects</a></li>
+          </ul>
+        </div>
+      </div>`;
 }
 
 export { navbar };
